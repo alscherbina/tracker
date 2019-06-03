5dd4ba95aefc2170ccdc870e38eaedd84d766635
Oleksandr_Shcherbyna <Oleksandr_Shcherbyna@epam.com>
2019-06-03 11:08:02 +0300

    Handling 403 error with proper message

--- src/client/store/actions/general.js
+++ src/client/store/actions/general.js
@@ -1,12 +1,16 @@
 import * as actionTypes from './actionTypes';
 
 const isAPIError = payload => payload.response && payload.response.data && payload.response.data.message;
+const isAuthError = payload => payload.response && payload.response.status === 403;
 
 export const generalShowError = error => {
   const payload = error || { code: 0, message: 'Error' };
   if (isAPIError(payload)) {
     payload.code = 0;
     payload.message = payload.response.data.message;
+  } else if (isAuthError(payload)) {
+    payload.code = 403;
+    payload.message = 'Access denied';
   }
   return {
     type: actionTypes.GENERAL_SHOW_ERROR,
